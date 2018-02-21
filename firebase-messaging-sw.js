47ff8d364a70ea1c2f88ef96118f7c8a62e09f84
Rakib <rakibul.islam@bs-23.net>
2018-02-21 19:01:53 +0600

    Initial commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+// Give the service worker access to Firebase Messaging.
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-messaging.js')
+
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+var config = {
+    messagingSenderId: "89996250717"
+};
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging()
+messaging.setBackgroundMessageHandler(function(payload) {
+  const notificationTitle = 'Message Title';
+  const notificationOptions = {
+    body: 'Message body',
+    icon: 'alarm.png'
+  };
+  
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
---
+++ script.js
@@ -0,0 +1,74 @@
+// Initialize the Firebase app by passing in the messagingSenderId
+var config = {
+  messagingSenderId: "89996250717"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+// Request for permission
+messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  messaging.getToken()
+  .then(function(currentToken) {
+    if (currentToken) {
+      console.log('Token: ' + currentToken)
+      sendTokenToServer(currentToken);
+    } else {
+      console.log('No Instance ID token available. Request permission to generate one.');
+      setTokenSentToServer(false);
+    }
+  })
+  .catch(function(err) {
+    console.log('An error occurred while retrieving token. ', err);
+    setTokenSentToServer(false);
+  });
+})
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+
+// Handle incoming messages
+messaging.onMessage(function(payload) {
+  console.log("Message received. ", payload);
+  toastr["info"](payload.notification.body, payload.notification.title);
+});
+
+// Callback fired if Instance ID token is updated.
+messaging.onTokenRefresh(function() {
+  messaging.getToken()
+  .then(function(refreshedToken) {
+    console.log('Token refreshed.');
+    // Indicate that the new Instance ID token has not yet been sent 
+    // to the app server.
+    setTokenSentToServer(false);
+    // Send Instance ID token to app server.
+    sendTokenToServer(refreshedToken);
+  })
+  .catch(function(err) {
+    console.log('Unable to retrieve refreshed token ', err);
+  });
+});
+
+// Send the Instance ID token your application server, so that it can:
+// - send messages back to this app
+// - subscribe/unsubscribe the token from topics
+function sendTokenToServer(currentToken) {
+  if (!isTokenSentToServer()) {
+    console.log('Sending token to server...');
+    // TODO(developer): Send the current token to your server.
+    setTokenSentToServer(true);
+  } else {
+    console.log('Token already sent to server so won\'t send it again ' +
+        'unless it changes');
+  }
+}
+
+function isTokenSentToServer() {
+  return window.localStorage.getItem('sentToServer') == 1;
+}
+
+function setTokenSentToServer(sent) {
+  window.localStorage.setItem('sentToServer', sent ? 1 : 0);
+}
